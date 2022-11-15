d89df8cbef88ffaf08970e0f9c9187e12cce3b2d
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-14 22:15:52 -0500

    Added images

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      Hello World!
+      <div>
+        <h2>IT support</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="bg-cyan-100 p-5 rounded-xl">
+              <img src="/courses/GoogleITSupport.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
