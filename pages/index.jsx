c33ec0bfea97ee97b47b684b6b5ea751cddd62b8
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-21 00:35:39 -0500

    added horizontal scroll bar & hide with tailwind plugin

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import CourseTemplate from "../components/courseTemplate";
 
 export default function Home() {
   const [courseInfo, setCourseInfo] = useState([]);
+  const [phrase, setPhrase] = useState("");
 
   useEffect(() => {
     fetch("/api/courses")
@@ -16,19 +17,28 @@ export default function Home() {
 
   return (
     <div className="p-5">
+      <input
+        value={phrase}
+        onChange={(e) => setPhrase(e.target.value)}
+        type="text"
+        placeholder="Search for courses..."
+        className="bg-gray-100 py-2 px-4 rounded-xl w-full"
+      />
       <div>
         {courseCategoryNames.map((categoryName) => (
           <div key={categoryName}>
-            <h2 className="text-2xl capitalize font-semibold ">
+            <h2 className="text-2xl py-5 capitalize font-semibold ">
               {categoryName}
             </h2>
-            {courseInfo
-              .filter((c) => c.category === categoryName)
-              .map((courseInfo) => (
-                <div>
-                  <CourseTemplate {...courseInfo} />
-                </div>
-              ))}
+            <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+              {courseInfo
+                .filter((c) => c.category === categoryName)
+                .map((courseInfo) => (
+                  <div key={courseInfo._id} className="px-5 snap-start">
+                    <CourseTemplate {...courseInfo} />
+                  </div>
+                ))}
+            </div>
           </div>
         ))}
         <div className="py-4"></div>
